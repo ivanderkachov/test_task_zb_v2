6fb20d94f892bae870d4f96e12d62fd2f46aaf33
Деркачев Иван <derkachov_ivan@host-192-168-0-103.sohonet.ua>
2022-10-09 11:51:40 +0300

    styled components added

--- src/components/form/form.js
+++ src/components/form/form.js
@@ -1,7 +1,97 @@
 import React, { useState } from "react"
 import { useDispatch } from "react-redux"
+import styled from 'styled-components'
 
 import { addMessage } from "../../redux/reducers/reducer";
+
+
+const StyledForm = styled.form`
+  display: flex;
+  border: 1px solid #dcdcdc;
+  flex-direction: column;
+  width: 563px;
+  padding: 3px;
+  color: #3e3e3e;
+`;
+const StyledFormHeader = styled.h1`
+  font-style: normal;
+  font-weight: 400;
+  font-size: 40px;
+  color: #3e3e3e;
+`;
+const StyledFormInputWrapper = styled.div`
+  align-items: center;
+`;
+const StyledInputName = styled.input`
+  background: #ffffff;
+  box-sizing:border-box;
+  border: 1px solid #dcdcdc;
+  border-radius: 10px;
+  width: 557px;
+  height: 93px;
+  margin: 6px 3px;
+  padding-left: 46px;
+  font-style: normal;
+  font-weight: 400;
+  font-size: 18px;
+  ::placeholder {
+    color: #2d2d2d;
+  }
+`;
+
+const StyledInputEmail = styled.input`
+  background: #ffffff;
+  box-sizing: border-box;
+  border: 1px solid #dcdcdc;
+  border-radius: 10px;
+  width: 557px;
+  height: 93px;
+  margin: 6px 3px;
+  padding-left: 46px;
+  font-style: normal;
+  font-weight: 400;
+  font-size: 18px;
+  ::placeholder {
+    color: #2d2d2d;
+  }
+`;
+const StyledInputMessage = styled.textarea`
+  background: #ffffff;
+  box-sizing: border-box;
+  border: 1px solid #dcdcdc;
+  border-radius: 10px;
+  width: 557px;
+  height: 189px;
+  margin: 6px 3px;
+  padding-left: 46px;
+  padding-top: 31px;
+  resize: none;
+  font-family: inherit;
+  font-size: inherit;
+  font-style: normal;
+  font-weight: 400;
+  font-size: 18px;
+  ::placeholder {
+    color: #2d2d2d;
+  }
+`;
+
+const StyledButton = styled.button`
+  width: 218px;
+  height: 73px;
+  background: #fad34f;
+  border: none;
+  border-radius: 500px;
+  font-weight: 400;
+  font-size: 18px;
+
+  color: #ffffff;
+  margin-top: 12px;
+  cursor: pointer;
+  :hover {
+    background: #d8d33d;
+  }
+`;
 
 const Form = ({ toggle, setToggle }) => {
   const dispatch = useDispatch()
@@ -29,18 +119,35 @@ const Form = ({ toggle, setToggle }) => {
 
 
   return (
-    <form>
-      <h2>Header</h2>
-      <label htmlFor="name">Name</label>
-      <input type="text" name="name" value={messageForm.name} onChange={onChangeHandler} />
-      <label htmlFor="email">Email</label>
-      <input type="email" name="email" value={messageForm.email} onChange={onChangeHandler} />
-      <label htmlFor="message">Message</label>
-      <input name="message" value={messageForm.message} onChange={onChangeHandler} />
-      <button type="submit" onClick={buttonHandler}>
+    <StyledForm>
+      <StyledFormHeader>Reach out to us!</StyledFormHeader>
+      <StyledFormInputWrapper>
+        <StyledInputName
+          type="text"
+          name="name"
+          placeholder="Your name*"
+          value={messageForm.name}
+          onChange={onChangeHandler}
+        />
+        <StyledInputEmail
+          type="email"
+          name="email"
+          placeholder="Your e-mail*"
+          value={messageForm.email}
+          onChange={onChangeHandler}
+        />
+        <StyledInputMessage
+          name="message"
+          placeholder="Your message*"
+          value={messageForm.message}
+          onChange={onChangeHandler}
+        />
+      </StyledFormInputWrapper>
+
+      <StyledButton type="submit" onClick={buttonHandler}>
         Send message
-      </button>
-    </form>
+      </StyledButton>
+    </StyledForm>
   );
 }
 
--- src/pages/main/main.js
+++ src/pages/main/main.js
@@ -1,9 +1,48 @@
 import React, { useState, useEffect } from "react";
 import { useSelector, useDispatch } from "react-redux";
+import styled, { createGlobalStyle } from "styled-components";
 
 import { getALLMessages } from "../../redux/reducers/reducer";
 import Form from "../../components/form/form";
 import Footer from "../../components/footer/footer";
+import Ellipse from '../../images/Ellipse 31.png'
+
+const GlobalStyle = createGlobalStyle`
+  html {
+    height: 100vh;
+  }
+  body {
+    font-family: Apercu Arabic Pro, sans-serif;
+    height: 100%;
+    margin: 0;
+    position: relative;
+  }
+`
+const StyledFormContainer = styled.div`
+  position: absolute;
+  left: 147px;
+  top: 179px;
+`;
+
+const StyledFooterContainer = styled.div`
+  position: absolute;
+  left: 0px;
+  top: 928px;
+  width: 100%;
+  height: 200px;
+  background: #fafafa;
+  border: 1px solid #d8d8d8;
+`;
+const StyledEllipse = styled.img`
+  position: absolute;
+  border-radius: 50%;
+  width: 87px;
+  height: 87px;
+  left: 714px;
+  top: 21px;
+  background: #fad34f;
+  opacity: 0.1;
+`;
 
 const Main = () => {
 
@@ -19,8 +58,14 @@ const Main = () => {
 
   return (
     <>
-    <Form toggle={toggle} setToggle={setToggle} />
-    <Footer />
+      <GlobalStyle />
+      <StyledFormContainer>
+        <Form toggle={toggle} setToggle={setToggle} />
+      </StyledFormContainer>
+      <StyledFooterContainer>
+        <Footer />
+      </StyledFooterContainer>
+      <StyledEllipse src={Ellipse} alt="Ellipse" />
     </>
   );
 }
